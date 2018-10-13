d7f836f74a93fcedcd9f629006532a4e0773199b
arsoto <sotorojasarturo@gmail.com>
2018-10-13 15:58:04 -0300

    ejercicio 3 de guia 3-1 nodo crados

--- guia3-1-3/Nodos.h
+++ guia3-1-3/Nodos.h
@@ -1,4 +1,21 @@
+#include <string>
+
+using namespace std;
+
 #ifndef NODOS_H
 #define NODOS_H
+
+typedef struct _Ingrediente{
+
+    string nombre;
+    struct _Ingrediente *sig;
+
+}Ingrediente;
+
+typedef struct _Nodo {
+    string nombre;
+    struct _Nodo *sigNodo;
+    struct _Ingredientes *sigIngre;
+} Nodo;
 
 #endif
